b97096bca5b72516c1c27b00e6b2c025b4efea27
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-13 08:54:43 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -20,15 +20,15 @@ if env:
     api_id = os.environ.get("api_id")
     api_hash = os.environ.get("api_hash")
 else:
-    OWNER_ID = 6765826972
-    sudo_users = ["6765826972", "6046482147"]
+    OWNER_ID = 6942997609
+    sudo_users = ["6942997609", "6046482147"]
     GROUP_ID = -100
-    TOKEN = ""
+    TOKEN = "6695037673:AAFxvZkhSf9K0M29U_cL-cgeiZYGnZoeVOY"
     mongo_url = "mongodb://localhost:27017/"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = -100
-    api_id = 123
-    api_hash = ""
+    CHARA_CHANNEL_ID = -1002081390216
+    api_id = 20756810
+    api_hash = "7af61b35db3a1c79f7e4c727fa95831e"
