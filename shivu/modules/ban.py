ffb4b5ea4312cea200dd52531a9690e334d33619
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-17 22:46:37 +0530

    Update ban.py

--- shivu/modules/ban.py
+++ shivu/modules/ban.py
@@ -1,6 +1,6 @@
 import pymongo
 from pyrogram import Client, filters
-from config import OWNER_ID, BOT_TOKEN, API_ID, API_HASH
+from shivu import OWNER_ID, BOT_TOKEN, API_ID, API_HASH
 
 # Connect to MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
