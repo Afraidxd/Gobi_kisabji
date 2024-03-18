c76399d15faddde35e2958077040ed07444962ee
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-18 17:00:02 +0530

    Update give.py

--- shivu/modules/give.py
+++ shivu/modules/give.py
@@ -2,9 +2,8 @@ from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 from pymongo import MongoClient
 from shivu import db, collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
-
-from shivu import application, OWNER_ID, mongo_url, collection 
-
+from shivu import application, shivuu, LOGGER 
+from shivu.modules import ALL_MODULES
 client = MongoClient(mongo_url)
 db = client["characters"]
 characters_collection = db["characters"]
