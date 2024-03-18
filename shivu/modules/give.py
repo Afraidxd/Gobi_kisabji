7d13285c40a5fabd997f3e6edba58c275c9ea225
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-18 15:45:37 +0530

    Update give.py

--- shivu/modules/give.py
+++ shivu/modules/give.py
@@ -7,30 +7,31 @@ from shivu import application, OWNER_ID, mongo_url
 client = MongoClient(mongo_url)
 db = client["characters"]
 characters_collection = db["characters"]
+
 async def give(update: Update, context: CallbackContext) -> None:
-    if update.effective_user.id != OWNER_ID:
-        await update.message.reply_text("You are not authorized to use this command.")
-        return
+    if update.effective_user.id != OWNER_ID:
+        await update.message.reply_text("You are not authorized to use this command.")
+        return
 
-    replied_user = update.message.reply_to_message.from_user
-    if replied_user is None:
-        await update.message.reply_text("Please reply to a user to give them a character.")
-        return
+    replied_user = update.message.reply_to_message.from_user
+    if replied_user is None:
+        await update.message.reply_text("Please reply to a user to give them a character.")
+        return
 
-    id = context.args[0] if context.args else None
+    id = context.args[0] if context.args else None
 
-    if id is None:
-        await update.message.reply_text("Please provide the ID number of the character to give.")
-        return
+    if id is None:
+        await update.message.reply_text("Please provide the ID number of the character to give.")
+        return
 
-    character = characters_collection.find_one({"id": id})
-    if character:
-        # Update the character's owner to the replied user's ID
-        characters_collection.update_one({"id": id}, {"$set": {"owner_id": replied_user.id}})
-        response = f"Character with ID '{id}' has been given to user '{replied_user.username}'."
-    else:
-        response = f"Character with ID '{id}' not found in the database."
+    character = characters_collection.find_one({"id": id})
+    if character:
+        # Update the character's owner to the replied user's ID
+        characters_collection.update_one({"id": id}, {"$set": {"owner_id": replied_user.id}})
+        response = f"Character with ID '{id}' has been given to user '{replied_user.username}'."
+    else:
+        response = f"Character with ID '{id}' not found in the database."
 
-    await update.message.reply_text(response)
+    await update.message.reply_text(response)
 
 application.add_handler(CommandHandler("give", give))
