4d3c285ffba30d0a90bc5cef0c7456c664658bbb
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-18 08:52:07 +0530

    Update ban.py

--- shivu/modules/ban.py
+++ shivu/modules/ban.py
@@ -56,7 +56,7 @@ def list_blacklisted_users(bot: Bot, update: Update):
 bot = Bot(token=TOKEN)
 
 # Start the Bot
-update.start_polling()
+Update.start_polling()
 
 # Poll for updates and handle commands directly
 while True:
