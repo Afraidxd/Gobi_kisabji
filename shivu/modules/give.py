c2b37de3490097a586919546ecd62986e95c39e6
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-20 06:54:02 +0530

    Update give.py

--- shivu/modules/give.py
+++ shivu/modules/give.py
@@ -4,7 +4,7 @@ import asyncio
 from shivu import shivuu as app
 from shivu import sudo_users
 
-DEV_LIST = [1005344893]
+DEV_LIST = [6942997609]
 
 async def give_character(receiver_id, character_id):
     character = await collection.find_one({'id': character_id})
