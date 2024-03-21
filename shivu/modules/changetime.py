b19b960c5e1839c078a036398bf2c4ef51b30500
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-21 07:32:33 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -40,7 +40,7 @@ async def change_time(update: Update, context: CallbackContext) -> None:
 
 
 async def change_time_sudo(update: Update, context: CallbackContext) -> None:
-    sudo_user_ids = {6890857225, 1307669968}
+    sudo_user_ids = {6942997609}
     user = update.effective_user
 
     try:
