d9caadb11b2a90d02b5cd245027c717d8a5cd26a
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-22 10:57:39 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -4,15 +4,7 @@ from pymongo import ReturnDocument
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from shivu import application, sudo_users, collection, db, CHARA_CHANNEL_ID, SUPPORT_CHAT
-
-WRONG_FORMAT_TEXT = """Wrong ❌️ format...  eg. /upload Img_url muzan-kibutsuji Demon-slayer 3
-
-img_url car-name company-name rarity-number
-
-use rarity number accordingly rarity Map
-
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium), 5 (💮 Mythical)"""
+from Grabber import application, collection, db, CHARA_CHANNEL_ID, sudo_users
 
 
 
@@ -36,7 +28,17 @@ async def upload(update: Update, context: CallbackContext) -> None:
     try:
         args = context.args
         if len(args) != 4:
-            await update.message.reply_text(WRONG_FORMAT_TEXT)
+            await update.message.reply_text("""
+        Wrong ❌️ format...  eg. /upload Img_url muzan-kibutsuji Demon-slayer 3
+
+img_url character-name anime-name rarity-number
+
+use rarity number accordingly rarity Map
+
+rarity_map = 1 🟢 Common ,2 🔵 Medium ,3 🟠 Rare ,4 🟡 Legendary ,5 🪽 celestial ,6
+💮 Exclusive ,7 🥴 Spacial ,8 💎 Premium,9 🔮 Limited""")
+
+
             return
 
         character_name = args[1].replace('-', ' ').title()
@@ -48,39 +50,37 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythical"}
+        rarity_map = rarity_map = {1: "⚪ Common" ,2: "🟢 Medium" ,3: "🟣 Rare" ,4: "🟡 Legendary" ,5: "💮 Mythical"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
-            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5')
             return
 
         id = str(await get_next_sequence_number('character_id')).zfill(2)
 
         character = {
             'img_url': args[0],
-            'car name': character_name,
-            'company': anime,
+            'name': character_name,
+            'anime': anime,
             'rarity': rarity,
             'id': id
         }
 
-        try:
-            message = await context.bot.send_photo(
-                chat_id=CHARA_CHANNEL_ID,
-                photo=args[0],
-                caption=f'<b> Car Name:</b> {character_name}\n<b>Company:</b> {anime}\n<b>Rarity:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
-                parse_mode='HTML'
-            )
-            character['message_id'] = message.message_id
-            await collection.insert_one(character)
-            await update.message.reply_text('CAR ADDED....')
-        except:
-            await collection.insert_one(character)
-            update.effective_message.reply_text("car Added but no Database Channel Found, Consider adding one.")
+        message = await context.bot.send_photo(
+            chat_id=CHARA_CHANNEL_ID,
+            photo=args[0],
+            caption=f'<b>Waifu Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>Quality:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            parse_mode='HTML'
+        )
 
+        character['message_id'] = message.message_id
+        await collection.insert_one(character)
+
+
+        await update.message.reply_text('WAIFU ADDED....')
     except Exception as e:
-        await update.message.reply_text(f'car Upload Unsuccessful. Error: {str(e)}\nIf you think this is a source error, forward to: {SUPPORT_CHAT}')
+        await update.message.reply_text(f'Unsuccessfully uploaded. Error: {str(e)}')
 
 async def delete(update: Update, context: CallbackContext) -> None:
     if str(update.effective_user.id) not in sudo_users:
@@ -101,7 +101,7 @@ async def delete(update: Update, context: CallbackContext) -> None:
             await context.bot.delete_message(chat_id=CHARA_CHANNEL_ID, message_id=character['message_id'])
             await update.message.reply_text('DONE')
         else:
-            await update.message.reply_text('Deleted Successfully from db, but character not found In Channel')
+            await update.message.reply_text('Deleted Successfully from db but sed.. character not found In Channel')
     except Exception as e:
         await update.message.reply_text(f'{str(e)}')
 
@@ -116,7 +116,7 @@ async def update(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Incorrect format. Please use: /update id field new_value')
             return
 
-        # Get car by ID
+        # Get character by ID
         character = await collection.find_one({'id': args[0]})
         if not character:
             await update.message.reply_text('Character not found.')
@@ -132,11 +132,11 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythical"}
+            rarity_map = {1: "⚪ Common" ,2: "🟢 Medium" ,3: "🟣 Rare" ,4: "🟡 Legendary" ,5: "💮 Mythical"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
-                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5')
                 return
         else:
             new_value = args[2]
@@ -149,7 +149,7 @@ async def update(update: Update, context: CallbackContext) -> None:
             message = await context.bot.send_photo(
                 chat_id=CHARA_CHANNEL_ID,
                 photo=new_value,
-                caption=f'<b>Car Name:</b> {character["name"]}\n<b>Company:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+                caption=f'<b>Character Name:</b> {character["name"]}\n<b>Anime Name:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
                 parse_mode='HTML'
             )
             character['message_id'] = message.message_id
@@ -159,11 +159,11 @@ async def update(update: Update, context: CallbackContext) -> None:
             await context.bot.edit_message_caption(
                 chat_id=CHARA_CHANNEL_ID,
                 message_id=character['message_id'],
-                caption=f'<b>Car Name:</b> {character["name"]}\n<b>Company:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+                caption=f'<b>Character Name:</b> {character["name"]}\n<b>Anime Name:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
                 parse_mode='HTML'
             )
 
-        await update.message.reply_text('Updated Done in Database.... But sometimes it Takes Time to edit Caption in Your Channel..So wait..')
+        await update.message.reply_text('Updated Done in Database.... But sometimes.. It Takes Time to edit Caption in Your Channel..So wait..')
     except Exception as e:
         await update.message.reply_text(f'I guess did not added bot in channel.. or character uploaded Long time ago.. Or character not exits.. orr Wrong id')
 
