c306816321627400f36829f4989b8421b6b7eb51
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-22 11:08:53 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -69,7 +69,7 @@ rarity_map = (1: "⚪ Common" ,2: "🟢 Medium" ,3: "🟣 Rare" ,4: "🟡 Legend
         message = await context.bot.send_photo(
             chat_id=CHARA_CHANNEL_ID,
             photo=args[0],
-            caption=f'<b>Waifu Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>Quality:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            caption=f'<b>Car Name:</b> {character_name}\n<b>Anime Name:</b> {company}\n<b>Quality:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
             parse_mode='HTML'
         )
 
