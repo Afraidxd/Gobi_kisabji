55778d6ec694c3b0b6ee001fbb889c45abf5ef34
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-22 13:04:00 +0530

    Update race.py

--- shivu/modules/race.py
+++ shivu/modules/race.py
@@ -48,18 +48,17 @@ async def propose(update, context):
         rejection_photo_path = 'https://telegra.ph/file/561d51ab44101c27bc893.jpg'  # Replace with rejection photo path
         await update.message.reply_photo(photo=rejection_photo_path, caption=rejection_message)
     else:
-         random_reward = random.randint(30000, 90000)
-         monster_image = 'https://telegra.ph/file/f95f2d9755b89e16c7123.jpg'
-                      await
-user_collection.update_one(
-            {'id': user_id},
-            {'$inc': {'balance': random_reward}}
-        )
-        last_command_time[user_id] = datetime.utcnow()
-        await update.message.reply_photo(photo=monster_image, caption=f"Congratulations You won the race here is Your reward Ŧ{random_reward} tokens.")
+    random_reward = random.randint(30000, 90000)
+    monster_image = 'https://telegra.ph/file/f95f2d9755b89e16c7123.jpg'
+    await user_collection.update_one(
+        {'id': user_id},
+        {'$inc': {'balance': random_reward}}
+    )
+    last_command_time[user_id] = datetime.utcnow()
+    await update.message.reply_photo(photo=monster_image, caption=f"Congratulations You won the race here is Your reward Ŧ{random_reward} tokens.")
 
-    # Update last propose time
-    last_propose_times[user_id] = datetime.now()
+   # Update last propose time
+    last_propose_times[user_id] = datetime.now()
 
 # Add the CommandHandler to the application
 application.add_handler(CommandHandler("race", propose, block=False))
