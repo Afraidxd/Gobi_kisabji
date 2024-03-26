a0291e63befba0bc30ec32c4cd3133b20b4c09f2
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-26 09:59:17 +0530

    Update modules

---
+++ shivu/modules/challenge.py
@@ -0,0 +1,100 @@
+import importlib
+import time
+import random
+import re
+import asyncio
+from html import escape
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update
+from telegram.ext import CommandHandler,  CallbackContext, MessageHandler, CallbackQueryHandler, filters
+
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
+from shivu import application, LOGGER
+from shivu.modules import ALL_MODULES
+
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("shivu.modules." + module_name)
+
+
+last_user = {}
+warned_users = {}
+
+async def message_counter(update: Update, context: CallbackContext) -> None:
+    chat_id = str(update.effective_chat.id)
+    user_id = update.effective_user.id
+
+    if chat_id not in locks:
+        locks[chat_id] = asyncio.Lock()
+    lock = locks[chat_id]
+
+    async with lock:
+
+        chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
+        if chat_frequency:
+            message_frequency = chat_frequency.get('message_frequency', 100)
+        else:
+            message_frequency = 100
+
+        if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
+            last_user[chat_id]['count'] += 1
+            if last_user[chat_id]['count'] >= 10:
+
+                if user_id in warned_users and time.time() - warned_users[user_id] < 600:
+                    return
+                else:
+
+                    await update.message.reply_text(
+                        f"⚠️ Don't Spam {update.effective_user.first_name}...\nYour Messages Will be Ignored for 10 Minutes...")
+                    warned_users[user_id] = time.time()
+                    return
+        else:
+            last_user[chat_id] = {'user_id': user_id, 'count': 1}
+
+        if chat_id in message_counts:
+            message_counts[chat_id] += 1
+        else:
+            message_counts[chat_id] = 1
+
+        # Check if half an hour has passed since the last character was sent
+        if chat_id in last_character_time and time.time() - last_character_time[chat_id] >= 120:
+            await send_image(update, context)
+            last_character_time[chat_id] = time.time()
+
+        if message_counts[chat_id] % message_frequency == 0:
+            message_counts[chat_id] = 0
+
+async def send_image(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+
+    all_characters = list(await collection.find({'rarity': '🏎 Race edition}).to_list(length=None))
+
+    if chat_id not in sent_characters:
+        sent_characters[chat_id] = []
+
+    if len(sent_characters[chat_id]) == len(all_characters):
+        sent_characters[chat_id] = []
+
+    character = random.choice([c for c in all_characters if c['id'] not in sent_characters[chat_id]])
+
+    sent_characters[chat_id].append(character['id'])
+    last_characters[chat_id] = character
+
+    if chat_id in first_challenge_accept:
+        del first_challenge_accept[chat_id]
+
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo=character['img_url'],
+        caption=f"A New Common Car Appeared...\n/accept the Name and add it to Your Garage",
+        parse_mode='HTML',
+    )
+
