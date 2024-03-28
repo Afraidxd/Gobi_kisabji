94be34f44f664feffe47df04f90a0930b6d0afac
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-28 23:19:03 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from itertools import groupby
+import urllib.request
+import re
 import math
 from html import escape 
 import random
@@ -7,7 +9,7 @@ import random
 from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from shivu import collection, user_collection, application
+from Grabber import collection, user_collection, application
 
 async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user_id = update.effective_user.id
@@ -15,50 +17,59 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user = await user_collection.find_one({'id': user_id})
     if not user:
         if update.message:
-            await update.message.reply_text('Go and Grab Some car First....')
+            await update.message.reply_text('𝙔𝙤𝙪 𝙃𝙖𝙫𝙚 𝙉𝙤𝙩 𝙂𝙧𝙖𝙗 𝙖𝙣𝙮 𝙎𝙡𝙖𝙫𝙚 𝙔𝙚𝙩...')
         else:
-            await update.callback_query.edit_message_text('Go and Grab Some car First..')
+            await update.callback_query.edit_message_text('𝙔𝙤𝙪 𝙃𝙖𝙫𝙚 𝙉𝙤𝙩 𝙂𝙧𝙖𝙗 𝙖𝙣𝙮 𝙎𝙡𝙖𝙫𝙚 𝙔𝙚𝙩...')
         return
 
     characters = sorted(user['characters'], key=lambda x: (x['company'], x['id']))
 
     character_counts = {k: len(list(v)) for k, v in groupby(characters, key=lambda x: x['id'])}
 
+
     unique_characters = list({character['id']: character for character in characters}.values())
 
-    total_pages = math.ceil(len(unique_characters) / 15)
+
+    total_pages = math.ceil(len(unique_characters) / 7)  
 
     if page < 0 or page >= total_pages:
-        page = 0
+        page = 0  
 
-    harem_message = f"<b>{escape(update.effective_user.first_name)}'s Garage - Floor {page+1}/{total_pages}</b>\n"
+    harem_message = f"<b>{escape(update.effective_user.first_name)}'s Harem - Page {page+1}/{total_pages}</b>\n"
 
-    current_characters = unique_characters[page*15:(page+1)*15]
+
+    current_characters = unique_characters[page*7:(page+1)*7]
+
 
     current_grouped_characters = {k: list(v) for k, v in groupby(current_characters, key=lambda x: x['company'])}
 
     for anime, characters in current_grouped_characters.items():
-        harem_message += f'\n<b>{anime} {len(characters)}/{await collection.count_documents({"company": anime})}</b>\n'
+        harem_message += f'\n⥱ <b>{anime} {len(characters)}/{await collection.count_documents({"company": anime})}</b>\n'
 
         for character in characters:
-            count = character_counts[character['id']]
-            harem_message += f'{character["id"]} {character["car name"]} ×{count}\n'
+
+            count = character_counts[character['id']]  # Get the count from the character_counts dictionary
+            harem_message += f'➥{character["id"]}| {character["rarity"]} |{character["car name"]} ×{count}\n'
+
 
     total_count = len(user['characters'])
 
-    keyboard = [[InlineKeyboardButton(f"See Garage ({total_count})", switch_inline_query_current_chat=f"collection.{user_id}")]]
+    keyboard = [[InlineKeyboardButton(f"See Collection ({total_count})", switch_inline_query_current_chat=f"collection.{user_id}")]]
+
 
     if total_pages > 1:
+
         nav_buttons = []
         if page > 0:
-            nav_buttons.append(InlineKeyboardButton("⤾", callback_data=f"harem:{page-1}:{user_id}"))
+            nav_buttons.append(InlineKeyboardButton("⬅️", callback_data=f"harem:{page-1}:{user_id}"))
         if page < total_pages - 1:
-            nav_buttons.append(InlineKeyboardButton("⤿", callback_data=f"harem:{page+1}:{user_id}"))
+            nav_buttons.append(InlineKeyboardButton("➡️", callback_data=f"harem:{page+1}:{user_id}"))
         keyboard.append(nav_buttons)
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     if 'favorites' in user and user['favorites']:
+
         fav_character_id = user['favorites'][0]
         fav_character = next((c for c in user['characters'] if c['id'] == fav_character_id), None)
 
@@ -66,51 +77,63 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
             if update.message:
                 await update.message.reply_photo(photo=fav_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
             else:
+
                 if update.callback_query.message.caption != harem_message:
                     await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
         else:
             if update.message:
                 await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
             else:
+
                 if update.callback_query.message.text != harem_message:
                     await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
     else:
+
         if user['characters']:
+
             random_character = random.choice(user['characters'])
 
             if 'img_url' in random_character:
                 if update.message:
                     await update.message.reply_photo(photo=random_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
                 else:
+
                     if update.callback_query.message.caption != harem_message:
                         await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
             else:
                 if update.message:
                     await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
                 else:
+
                     if update.callback_query.message.text != harem_message:
                         await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
         else:
             if update.message:
-                await update.message.reply_text("Your Garage is Empty :)")
-                
+                await update.message.reply_text("Your List is Empty :)")
+
 
 async def harem_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     data = query.data
 
+
     _, page, user_id = data.split(':')
+
 
     page = int(page)
     user_id = int(user_id)
 
+
     if query.from_user.id != user_id:
-        await query.answer("It's Not Your Garage", show_alert=True)
+        await query.answer("This is not your Nigga ", show_alert=True)
         return
+
 
     await harem(update, context, page)
 
 
-application.add_handler(CommandHandler(["harem", "collection", "garage"], harem, block=False))
-harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
+
+
+application.add_handler(CommandHandler(["slaves"], harem,block=False))
+harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem.*', block=False)
 application.add_handler(harem_handler)
