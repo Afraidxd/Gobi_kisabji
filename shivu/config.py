7b4e166da9051a9194ae65cd641e4c7d42d79d37
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-29 13:08:28 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -14,14 +14,14 @@ if env:
 else:
     OWNER_ID = 6942997609
     sudo_users = ["6734369007", "6919722801", "5757833536", "5516379068", "6942997609", "7087814148"]
-    GROUP_ID = -1002081390216
+    GROUP_ID = -4115403338
     TOKEN = "7152549014:AAGQl39KqsCe7g8_oZO2tFa12tZOAIP75tY"
     mongo_url = "mongodb+srv://ishitaroy657boobs:6rhmZeIH8qESZmRS@waifu.hza810f.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/2ed20f2783ebb9cbfd04c.jpg", "https://telegra.ph/file/ed9d1302a7d58b2b2f2a2.jpg", "https://telegra.ph/file/578547d3f14979d82e1ae.jpg"]
     SUPPORT_CHAT = "carbotsupport"
     UPDATE_CHAT = "Divine_x_soul"
     BOT_USERNAME = "Grabyourcar_bot"
-    CHARA_CHANNEL_ID = -1002124319306
+    CHARA_CHANNEL_ID = -1002048802596
     LOGGER_ID = -1002081390216
     api_id = 20756810
     api_hash = "7af61b35db3a1c79f7e4c727fa95831e"
