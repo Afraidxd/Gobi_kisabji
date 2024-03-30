ef38df8edab7012be6d5c573dacb5d57114d3c1e
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-03-30 11:24:11 +0530

    Update wallet.py

--- shivu/modules/wallet.py
+++ shivu/modules/wallet.py
@@ -92,7 +92,7 @@ async def mtop(update, context):
         # Concatenate first_name and last_name if last_name is available
         full_name = f"{first_name} {last_name}" if last_name else first_name
 
-        top_users_message += f"{i}. <a href='tg://user?id={user_id}'>{full_name}</a>, 💸{user.get('balance', 0)} Tokens\n"
+        top_users_message += f"{i}. <a href="tg://user?id={user_id}">{full_name}</a>, 💸{user.get('balance', 0)} Tokens\n"
     # Send the photo and include the top_users_message in the caption
     photo_path = 'https://telegra.ph/file/14cb27c83d171bd125de4.jpg'
     await update.message.reply_photo(photo=photo_path, caption=top_users_message, parse_mode='HTML')
