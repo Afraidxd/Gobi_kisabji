220580e07e7cf0f7a747b4bfe59416d9d5b2302f
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-10 18:12:17 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -71,7 +71,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
 
         if query.startswith('collection.'):
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
-            user_anime_characters = sum(c['company'] == character['company"] for c in user['characters'])
+            user_anime_characters = sum(c['company'] == character['company'] for c in user['characters'])
             caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Car</b>\n\n: <b>{character['car name']} (x{user_character_count})</b>\n🏖️: <b>{character['company']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
         else:
             caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['car name']}</b>\n🏖️: <b>{character['company']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
