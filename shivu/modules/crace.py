5a223826c4ad3f0fc486fda3beaf7436b3329354
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-11 09:26:31 +0530

    Update crace.py

--- shivu/modules/crace.py
+++ shivu/modules/crace.py
@@ -2,6 +2,7 @@ from telegram.ext import CommandHandler
 from shivu import application, user_collection
 from telegram import Update
 import random
+import asyncio 
 
 participants = []
 race_started = False
