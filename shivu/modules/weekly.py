d86f87d2c24061e28c06a2710f0647e3a1d0dc40
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-14 11:13:30 +0530

    Update weekly.py

--- shivu/modules/weekly.py
+++ shivu/modules/weekly.py
@@ -4,33 +4,33 @@ from telegram import Update
 import random
 
 async def sbet(update, context):
-    # Parse the amount from the command
-    try:
-        amount = int(context.args[0])
-        if amount <= 0:
-            raise ValueError("Amount must be greater than zero.")
-    except (IndexError, ValueError):
-        await update.message.reply_text("Use /bet <amount>")
-        return
-    user_id = update.effective_user.id
-    user_balance = await user_collection.find_one({'id': user_id}, projection={'balance': 1})
+    # Parse the amount from the command
+    try:
+        amount = int(context.args[0])
+        if amount <= 0:
+            raise ValueError("Amount must be greater than zero.")
+    except (IndexError, ValueError):
+        await update.message.reply_text("Use /bet <amount>")
+        return
+    user_id = update.effective_user.id
+    user_balance = await user_collection.find_one({'id': user_id}, projection={'balance': 1})
 
-    if not user_balance or user_balance.get('balance', 0) < amount:
-        await update.message.reply_text("Insufficient balance to make the bet.")
-        return
-    if random.random() < 0.4:
-        won_amount = 2 * amount
-        await user_collection.update_one({'id': user_id}, {'$inc': {'balance': won_amount + amount}})
-        updated_balance = user_balance.get('balance', 0) + won_amount
-        await update.message.reply_text(
-            f"Congratulations You won!\n\n Your updated balance is {updated_balance}."
-        )
-    else:
-        await user_collection.update_one({'id': user_id}, {'$inc': {'balance': -amount}})
-        updated_balance = user_balance.get('balance', 0) - amount
-        await update.message.reply_text(
-            f"You lost {amount} .\n\nYour updated Balance is {updated_balance}."
-        )
+    if not user_balance or user_balance.get('balance', 0) < amount:
+        await update.message.reply_text("Insufficient balance to make the bet.")
+        return
+    if random.random() < 0.4:
+        won_amount = 2 * amount
+        await user_collection.update_one({'id': user_id}, {'$inc': {'balance': won_amount + amount}})
+        updated_balance = user_balance.get('balance', 0) + won_amount
+        await update.message.reply_text(
+            f"Congratulations You won!\n\n Your updated balance is {updated_balance}."
+        )
+    else:
+        await user_collection.update_one({'id': user_id}, {'$inc': {'balance': -amount}})
+        updated_balance = user_balance.get('balance', 0) - amount
+        await update.message.reply_text(
+            f"You lost {amount} .\n\nYour updated Balance is {updated_balance}."
+        )
 
 
 application.add_handler(CommandHandler("bet", sbet, block=False))
