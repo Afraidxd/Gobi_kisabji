05c6821f5e3d170ed2e64b8e23d65864f494e4d1
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-14 15:53:35 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -15,7 +15,7 @@ else:
     OWNER_ID = 7091293075
     sudo_users = ["6734369007", "6919722801", "5757833536", "5516379068", "7091293075", "7087814148"]
     GROUP_ID = -1002133316307
-    TOKEN = "6489718535:AAECD3sDuJZdzg4OhwOTpm8TEawAwioao_4"
+    TOKEN = "7152549014:AAHVU2la_6kIHX-yoXx9EfUGNUY8alKIJhQ"
     mongo_url = "mongodb+srv://Afraid:LhLMqfRcOadZMK4L@cluster0.g4r9uxr.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/2ed20f2783ebb9cbfd04c.jpg", "https://telegra.ph/file/ed9d1302a7d58b2b2f2a2.jpg", "https://telegra.ph/file/578547d3f14979d82e1ae.jpg"]
     SUPPORT_CHAT = "carbotsupport"
