b81c0015c4bccee6b71b50a5e87f05e357645722
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-15 20:07:26 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -109,10 +109,12 @@ async def send_image(update: Update, context: CallbackContext) -> None:
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
+
 async def button_click(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     car_name = last_characters.get(query.message.chat_id, {}).get('car name', 'Unknown Car')
     await query.answer(text=f"The car name is: {car_name}", show_alert=True)
+
 
 # In your main function or setup code
 application.add_handler(CallbackQueryHandler(button_click, pattern='^car_name$'))
