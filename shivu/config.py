b5d14f2c2bd11b9ba4a40209c1d01fc486b4ada4
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-18 11:56:37 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -16,7 +16,7 @@ else:
     sudo_users = ["6734369007", "6919722801", "5757833536", "5516379068", "7091293075", "7087814148"]
     GROUP_ID = -1002133316307
     TOKEN = "6887882596:AAG7nGne1KJxlgi8eeR18Yr4kWidpQMJx-I"
-    mongo_url = "mongodb+srv://RAICHU07:TheAltron@cluster0.fnxn8cv.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Afraid:LhLMqfRcOadZMK4L@cluster0.g4r9uxr.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/2ed20f2783ebb9cbfd04c.jpg", "https://telegra.ph/file/ed9d1302a7d58b2b2f2a2.jpg", "https://telegra.ph/file/578547d3f14979d82e1ae.jpg"]
     SUPPORT_CHAT = "carbotsupport"
     UPDATE_CHAT = "Divine_x_soul"
