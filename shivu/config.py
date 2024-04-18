7cef3cfc7d32f716ef8e57c20711f106c9cc918c
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-04-18 11:54:29 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -15,7 +15,7 @@ else:
     OWNER_ID = 7091293075
     sudo_users = ["6734369007", "6919722801", "5757833536", "5516379068", "7091293075", "7087814148"]
     GROUP_ID = -1002133316307
-    TOKEN = "7152549014:AAHVU2la_6kIHX-yoXx9EfUGNUY8alKIJhQ"
+    TOKEN = "6887882596:AAG7nGne1KJxlgi8eeR18Yr4kWidpQMJx-I"
     mongo_url = "mongodb+srv://RAICHU07:TheAltron@cluster0.fnxn8cv.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/2ed20f2783ebb9cbfd04c.jpg", "https://telegra.ph/file/ed9d1302a7d58b2b2f2a2.jpg", "https://telegra.ph/file/578547d3f14979d82e1ae.jpg"]
     SUPPORT_CHAT = "carbotsupport"
