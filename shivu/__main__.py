96450108205373dcc5d17b7c4bbb7066be6f48c6
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-15 23:22:29 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -3,17 +3,16 @@ import time
 import random
 import re
 import asyncio
-from html import escape 
+from html import escape
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
 
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
-from shivu import application, LOGGER 
+from Grabber import collection,Grabberu, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
+from shivu import application, LOGGER
 from shivu.modules import ALL_MODULES
-
+from shivu.modules.ptb_wordle import cwf
 
 locks = {}
 message_counters = {}
@@ -23,19 +22,21 @@ sent_characters = {}
 first_correct_guesses = {}
 message_counts = {}
 
-
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("shivu.modules." + module_name)
 
 
 last_user = {}
 warned_users = {}
+
+
 def escape_markdown(text):
     escape_chars = r'\*_`\\~>#+-=|{}.!'
     return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
 
 
 async def message_counter(update: Update, context: CallbackContext) -> None:
+    await cwf(update, context)
     chat_id = str(update.effective_chat.id)
     user_id = update.effective_user.id
 
@@ -51,7 +52,6 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         else:
             message_frequency = 100
 
-
         if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
             last_user[chat_id]['count'] += 1
             if last_user[chat_id]['count'] >= 10:
@@ -60,23 +60,24 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
                     return
                 else:
 
-                    await update.message.reply_text(f"⚠️ 𝘿𝙤𝙣'𝙩 𝙎𝙥𝙖𝙢 {update.effective_user.first_name}...\n𝙔𝙤𝙪𝙧 𝙈𝙚𝙨𝙨𝙖𝙜𝙚𝙨 𝙒𝙞𝙡𝙡 𝙗𝙚 𝙞𝙜𝙣𝙤𝙧𝙚𝙙 𝙛𝙤𝙧 10 𝙈𝙞𝙣𝙪𝙩𝙚𝙨...")
+                    await update.message.reply_text(
+                        f"⚠️ Don't Spam {update.effective_user.first_name}...\nYour Messages Will be Ignored for 10 Minutes...")
                     warned_users[user_id] = time.time()
                     return
         else:
             last_user[chat_id] = {'user_id': user_id, 'count': 1}
-
 
         if chat_id in message_counts:
             message_counts[chat_id] += 1
         else:
             message_counts[chat_id] = 1
 
-
         if message_counts[chat_id] % message_frequency == 0:
             await send_image(update, context)
 
             message_counts[chat_id] = 0
+
+
 async def send_image(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
 
@@ -96,17 +97,25 @@ async def send_image(update: Update, context: CallbackContext) -> None:
     if chat_id in first_correct_guesses:
         del first_correct_guesses[chat_id]
 
-    keyboard = [[InlineKeyboardButton("Name 🔥", callback_data='name')]]
+    name = character['name']
+
     await context.bot.send_photo(
         chat_id=chat_id,
         photo=character['img_url'],
-        caption=f"""A New {character['rarity']} Car Appeared...\n/guess Name and add in Your Garage""",
-        parse_mode='HTML')
+        caption=f"A New {character['rarity']} slave Appeared...\n\nName: {name} \n\n/grab the Name and add it to Your slave list",
+        parse_mode='HTML'
+    )
+
 
 async def button_click(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-    name = last_characters.get(query.message.chat_id, {}).get('car name', 'Unknown car')
-    await query.answer(text=f"The car name is: {name}", show_alert=True)
+    name = last_characters.get(query.message.chat_id, {}).get('name', 'Unknown slave')
+    spoilered_name = f'||{name}||'  # Wrap the name within spoiler tags
+    await query.answer(text=f"The slave name is: {spoilered_name}", show_alert=True)
+
+
+# In your main function or setup code
+application.add_handler(CallbackQueryHandler(button_click, pattern='^name$'))
 
 
 async def guess(update: Update, context: CallbackContext) -> None:
@@ -126,11 +135,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("𝙉𝙖𝙝𝙝 𝙔𝙤𝙪 𝘾𝙖𝙣'𝙩 𝙪𝙨𝙚 𝙏𝙝𝙞𝙨 𝙏𝙮𝙥𝙚𝙨 𝙤𝙛 𝙬𝙤𝙧𝙙𝙨 ❌️")
         return
 
-
-    name_parts = last_characters[chat_id]['car name'].lower().split()
+    name_parts = last_characters[chat_id]['name'].lower().split()
 
     if sorted(name_parts) == sorted(guess.split()) or any(part == guess for part in name_parts):
-
 
         first_correct_guesses[chat_id] = user_id
 
@@ -154,7 +161,6 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 'characters': [last_characters[chat_id]],
             })
 
-
         group_user_total = await group_user_totals_collection.find_one({'user_id': user_id, 'group_id': chat_id})
         if group_user_total:
             update_fields = {}
@@ -176,8 +182,6 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 'count': 1,
             })
 
-
-
         group_info = await top_global_groups_collection.find_one({'group_id': chat_id})
         if group_info:
             update_fields = {}
@@ -195,56 +199,50 @@ async def guess(update: Update, context: CallbackContext) -> None:
                 'count': 1,
             })
 
+        keyboard = [[InlineKeyboardButton(f"Slaves 🔥", switch_inline_query_current_chat=f"collection.{user_id}")]]
 
-
-        keyboard = [[InlineKeyboardButton(f"𝙂𝙖𝙧𝙖𝙜𝙚 🔥", switch_inline_query_current_chat=f"collection.{user_id}")]]
-
-
-        await update.message.reply_text(f'<b><a href="tg://user?id={user_id}">{escape(update.effective_user.first_name)}</a></b> 𝙔𝙤𝙪 𝙂𝙤𝙩 𝙉𝙚𝙬 𝘾𝙖𝙧🫧 \n🌸𝗡𝗔𝗠𝗘: <b>{last_characters[chat_id]["car name"]}</b> \n🧩𝘾𝙤𝙢𝙥𝙖𝙣𝙮: <b>{last_characters[chat_id]["company"]}</b> \n𝗥𝗔𝗜𝗥𝗧𝗬: <b>{last_characters[chat_id]["rarity"]}</b>\n\n⛩ 𝘾𝙝𝙚𝙘𝙠 𝙮𝙤𝙪𝙧 /collection 𝙉𝙤𝙬', parse_mode='HTML', reply_markup=InlineKeyboardMarkup(keyboard))
+        await update.message.reply_text(f'<b><a href="tg://user?id={user_id}">{escape(update.effective_user.first_name)}</a></b> 𝙔𝙤𝙪 𝙂𝙤𝙩 𝙉𝙚𝙬 Slave🫧 \n🌸𝗡𝗔𝗠𝗘: <b>{last_characters[chat_id]["name"]}</b> \n🧩𝘾𝙤𝙢𝙥𝙖𝙣𝙮: <b>{last_characters[chat_id]["anime"]}</b> \n𝗥𝗔𝗜𝗥𝗧𝗬: <b>{last_characters[chat_id]["rarity"]}</b>\n\n⛩ 𝘾𝙝𝙚𝙘𝙠 𝙮𝙤𝙪𝙧 /slaves 𝙉𝙤𝙬', parse_mode='HTML', reply_markup=InlineKeyboardMarkup(keyboard))
 
     else:
         await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙧𝙞𝙩𝙚 𝘾𝙤𝙧𝙧𝙚𝙘𝙩 𝙉𝙖𝙢𝙚... ❌️')
 
+
 async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
 
-
     if not context.args:
-        await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙥𝙧𝙤𝙫𝙞𝙙𝙚 𝘾𝙖𝙧 𝙞𝙙...')
+        await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙥𝙧𝙤𝙫𝙞𝙙𝙚 Slave 𝙞𝙙...')
         return
 
     character_id = context.args[0]
 
-
     user = await user_collection.find_one({'id': user_id})
     if not user:
-        await update.message.reply_text('𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤𝙩 𝙂𝙤𝙩 𝘼𝙣𝙮 𝘾𝙖𝙧 𝙮𝙚𝙩...')
+        await update.message.reply_text('𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤𝙩 𝙂𝙤𝙩 𝘼𝙣𝙮 Slave 𝙮𝙚𝙩...')
         return
-
 
     character = next((c for c in user['characters'] if c['id'] == character_id), None)
     if not character:
-        await update.message.reply_text('𝙏𝙝𝙞𝙨 𝘾𝙖𝙧 𝙞𝙨 𝙉𝙤𝙩 𝙄𝙣 𝙮𝙤𝙪𝙧 𝙂𝙖𝙧𝙖𝙜𝙚')
+        await update.message.reply_text('𝙏𝙝𝙞𝙨 slave 𝙞𝙨 𝙉𝙤𝙩 𝙄𝙣 𝙮𝙤𝙪𝙧 list')
         return
-
 
     user['favorites'] = [character_id]
 
-
     await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}})
 
-    await update.message.reply_text(f'🥳𝘾𝙖𝙧 {character["car name"]} 𝙄𝙨 𝙎𝙚𝙩 𝙤𝙣 𝙔𝙤𝙪𝙧 𝙁𝙞𝙧𝙨𝙩 𝙛𝙡𝙤𝙤𝙧 𝙣𝙤𝙬...')
+    await update.message.reply_text(f'🥳slave {character["name"]} is your favorite 𝙣𝙤𝙬...')
 
 
 def main() -> None:
     """Run bot."""
 
-    application.add_handler(CommandHandler(["guess"], guess, block=False))
-    application.add_handler(CommandHandler("fav", fav, block=False))
+    application.add_handler(CommandHandler(["grab"], guess, block=False))
+    application.add_handler(CommandHandler("marry", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
     application.run_polling(drop_pending_updates=True)
 
+
 if __name__ == "__main__":
-    shivuu.start()
-    LOGGER.info("Bot started")
+    shivu.start()
+    LOGGER.info("lund started")
     main()
