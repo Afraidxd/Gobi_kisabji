959222ca81f9a448de356bffbe678d0e48f0c331
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-16 11:33:24 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -6,7 +6,9 @@ import asyncio
 from html import escape
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import Update, updater
+from telegram import Update
+from telegram.ext import Updater, CallbackQueryHandler
+
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
 
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
