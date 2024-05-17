22b9fd5cc354658cfa4402614d1104e2daa27680
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-17 13:33:23 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -117,4 +117,4 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
         return
     await harem(update, context, page)
 
-application.add_handler(CommandHandler(["slaves"], harem, block=False))
+application.add_handler(CommandHandler(["collection"], harem, block=False))
