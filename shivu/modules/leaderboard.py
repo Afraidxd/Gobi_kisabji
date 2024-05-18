f3856871c1b7332ff75570f95e4fd6f57c52daaa
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-18 23:19:55 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -5,18 +5,15 @@ import html
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from shivu import (application, PHOTO_URL, OWNER_ID,
-                    user_collection, top_global_groups_collection, top_global_groups_collection, 
-                    group_user_totals_collection)
+from shivu import (
+    application, PHOTO_URL, OWNER_ID,
+    user_collection, top_global_groups_collection,
+    group_user_totals_collection
+)
 
-from shivu import sudo_users as SUDO_USERS 
-
-
-
-
+from shivu import sudo_users as SUDO_USERS
 
 async def global_leaderboard(update: Update, context: CallbackContext) -> None:
-
     cursor = top_global_groups_collection.aggregate([
         {"$project": {"group_name": 1, "count": 1}},
         {"$sort": {"count": -1}},
@@ -28,15 +25,12 @@ async def global_leaderboard(update: Update, context: CallbackContext) -> None:
 
     for i, group in enumerate(leaderboard_data, start=1):
         group_name = html.escape(group.get('group_name', 'Unknown'))
-
-        if len(group_name) > 10:
+        if len(group_name) > 15:
             group_name = group_name[:15] + '...'
         count = group['count']
         leaderboard_message += f'{i}. <b>{group_name}</b> ➾ <b>{count}</b>\n'
 
-
     photo_url = random.choice(PHOTO_URL)
-
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
 async def ctop(update: Update, context: CallbackContext) -> None:
@@ -50,24 +44,20 @@ async def ctop(update: Update, context: CallbackContext) -> None:
     ])
     leaderboard_data = await cursor.to_list(length=10)
 
-    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n"
+    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIMES IN THIS GROUP</b>\n\n"
 
     for i, user in enumerate(leaderboard_data, start=1):
         username = user.get('username', 'Unknown')
         first_name = html.escape(user.get('first_name', 'Unknown'))
-
-        if len(first_name) > 10:
+        if len(first_name) > 15:
             first_name = first_name[:15] + '...'
         character_count = user['character_count']
         leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
 
     photo_url = random.choice(PHOTO_URL)
-
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
-
 async def leaderboard(update: Update, context: CallbackContext) -> None:
-
     cursor = user_collection.aggregate([
         {"$project": {"username": 1, "first_name": 1, "character_count": {"$size": "$characters"}}},
         {"$sort": {"character_count": -1}},
@@ -80,24 +70,14 @@ async def leaderboard(update: Update, context: CallbackContext) -> None:
     for i, user in enumerate(leaderboard_data, start=1):
         username = user.get('username', 'Unknown')
         first_name = html.escape(user.get('first_name', 'Unknown'))
-
-        if len(first_name) > 10:
+        if len(first_name) > 15:
             first_name = first_name[:15] + '...'
         character_count = user['character_count']
         leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
 
     photo_url = random.choice(PHOTO_URL)
-
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
-
-
-
-
 application.add_handler(CommandHandler('TopGroups', global_leaderboard, block=False))
-
-
-
-application.add_handler(CommandHandler('top', leaderboard, block=False))
-
 application.add_handler(CommandHandler('ctop', ctop, block=False))
+application.add_handler(CommandHandler('top', leaderboard, block=False))
