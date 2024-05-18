b79366239bd1b51c4eef3b7b8057e73387ae2424
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-18 23:10:41 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -91,6 +91,33 @@ async def leaderboard(update: Update, context: CallbackContext) -> None:
 
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
+async def ctop(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+
+    cursor = group_user_totals_collection.aggregate([
+        {"$match": {"group_id": chat_id}},
+        {"$project": {"username": 1, "first_name": 1, "character_count": "$count"}},
+        {"$sort": {"character_count": -1}},
+        {"$limit": 10}
+    ])
+    leaderboard_data = await cursor.to_list(length=10)
+
+    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n"
+
+    for i, user in enumerate(leaderboard_data, start=1):
+        username = user.get('username', 'Unknown')
+        first_name = html.escape(user.get('first_name', 'Unknown'))
+
+        if len(first_name) > 10:
+            first_name = first_name[:15] + '...'
+        character_count = user['character_count']
+        leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
+
+    photo_url = random.choice(PHOTO_URL)
+
+    await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
+
+
 
 
 
@@ -98,4 +125,6 @@ application.add_handler(CommandHandler('TopGroups', global_leaderboard, block=Fa
 
 
 
-application.add_handler(CommandHandler('ctop', leaderboard, block=False))
+application.add_handler(CommandHandler('top', leaderboard, block=False))
+
+application.add_handler(CommandHandler('ctop', ctop, block=False))
