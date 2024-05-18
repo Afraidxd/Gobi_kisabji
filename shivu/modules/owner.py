8ca317f116a0266c34fe549bf3d422b3676062b4
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-18 22:55:59 +0530

    Update owner.py

--- shivu/modules/owner.py
+++ shivu/modules/owner.py
@@ -2,94 +2,95 @@ import os
 import random
 import html
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
 
 from shivu import (
     application, PHOTO_URL, OWNER_ID,
     user_collection, top_global_groups_collection, group_user_totals_collection,
     sudo_users as SUDO_USERS
 )
-async def broadcast(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) == OWNER_ID:
-        if update.message.reply_to_message is None:
-            await update.message.reply_text('Please reply to a message to broadcast.')
-            return
 
-        all_users = await user_collection.find({}).to_list(length=None)
-        all_groups = await group_user_totals_collection.find({}).to_list(length=None)
+photo = random.choice(PHOTO_URL)
 
-        unique_user_ids = set(user['id'] for user in all_users if 'id' in user)
-        unique_group_ids = set(group['group_id'] for group in all_groups)
+async def global_leaderboard(update: Update, context: CallbackContext) -> None:
+    cursor = top_global_groups_collection.aggregate([
+        {"$project": {"group_name": 1, "count": 1}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    leaderboard_data = await cursor.to_list(length=10)
 
-        total_sent = 0
-        total_failed = 0
+    leaderboard_message = "<b>TOP 10 GROUPS WHO GUESSED MOST CHARACTERS</b>\n\n"
 
-        for user_id in unique_user_ids:
-            try:
-                await context.bot.forward_message(chat_id=user_id, from_chat_id=update.effective_chat.id, message_id=update.message.reply_to_message.message_id)
-                total_sent += 1
-            except Exception:
-                total_failed += 1
+    for i, group in enumerate(leaderboard_data, start=1):
+        group_name = html.escape(group.get('group_name', 'Unknown'))
 
-        for group_id in unique_group_ids:
-            try:
-                await context.bot.forward_message(chat_id=group_id, from_chat_id=update.effective_chat.id, message_id=update.message.reply_to_message.message_id)
-                total_sent += 1
-            except Exception:
-                total_failed += 1
+        if len(group_name) > 10:
+            group_name = group_name[:15] + '...'
+        count = group['count']
+        leaderboard_message += f'{i}. <b>{group_name}</b> ➾ <b>{count}</b>\n'
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f'Broadcast report:\n\nTotal messages sent successfully: {total_sent}\nTotal messages failed to send: {total_failed}'
-        )
-    else:
-        await update.message.reply_text('Only Murat Can use')
+    photo_url = random.choice(photo)
 
-async def stats(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in OWNER_ID:
-        update.message.reply_text('only For Sudo users...')
-        return
+    await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
-    user_count = await user_collection.count_documents({}) + 500
-    group_count = len(await group_user_totals_collection.distinct('group_id')) + 350
+async def ctop(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
 
-    await update.message.reply_text(f'Total Users: {user_count}\nTotal groups: {group_count}')
+    cursor = group_user_totals_collection.aggregate([
+        {"$match": {"group_id": chat_id}},
+        {"$project": {"username": 1, "first_name": 1, "character_count": "$count"}},
+        {"$sort": {"character_count": -1}},
+        {"$limit": 10}
+    ])
+    leaderboard_data = await cursor.to_list(length=10)
 
-async def send_users_document(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in SUDO_USERS:
-        update.message.reply_text('only For Sudo users...')
-        return
+    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n"
 
-    cursor = user_collection.find({})
-    users = [document async for document in cursor]
-    user_list = '\n'.join(user.get('first_name', 'Unknown') for user in users)
+    for i, user in enumerate(leaderboard_data, start=1):
+        username = user.get('username', 'Unknown')
+        first_name = html.escape(user.get('first_name', 'Unknown'))
 
-    with open('users.txt', 'w') as f:
-        f.write(user_list)
+        if len(first_name) > 10:
+            first_name = first_name[:15] + '...'
+        character_count = user['character_count']
+        leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
 
-    with open('users.txt', 'rb') as f:
-        await context.bot.send_document(chat_id=update.effective_chat.id, document=f)
+    photo_url = random.choice(photo)
 
-    os.remove('users.txt')
+    await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
 
-async def send_groups_document(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in SUDO_USERS:
-        update.message.reply_text('Only For Sudo users...')
-        return
+async def leaderboard(update: Update, context: CallbackContext) -> None:
+    cursor = user_collection.aggregate([
+        {"$match": {"characters": {"$exists": True, "$type": "array"}}},
+        {"$project": {"username": 1, "first_name": 1, "character_count": {"$size": "$characters"}}},
+        {"$sort": {"character_count": -1}},
+        {"$limit": 10}
+    ])
 
-    cursor = top_global_groups_collection.find({})
-    groups = [document async for document in cursor]
-    group_list = '\n'.join(group['group_name'] for group in groups)
+    leaderboard_data = await cursor.to_list(length=10)
 
-    with open('groups.txt', 'w') as f:
-        f.write(group_list)
+    leaderboard_message = "<b>TOP 10 USERS WITH MOST CHARACTERS</b>\n\n"
 
-    with open('groups.txt', 'rb') as f:
-        await context.bot.send_document(chat_id=update.effective_chat.id, document=f)
+    for i, user in enumerate(leaderboard_data, start=1):
+        username = user.get('username', 'Unknown')
+        first_name = html.escape(user.get('first_name', 'Unknown'))
 
-    os.remove('groups.txt')
-application.add_handler(CommandHandler('list', send_users_document, block=False))
-application.add_handler(CommandHandler('groups', send_groups_document, block=False))
-application.add_handler(CommandHandler('stats', stats, block=False))
-application.add_handler(CommandHandler('broadcast', broadcast, block=False))
+        if len(first_name) > 10:
+            first_name = first_name[:15] + '...'
+        character_count = user['character_count']
+        leaderboard_message += f'{i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
+
+    photo = [
+        'https://telegra.ph/file/7f37afa1b7c3e035dff78.jpg',
+        'https://telegra.ph/file/4555dc127a0012abf8506.jpg'
+    ]
+
+    photo_url = random.choice(photo)
+
+    await update.message.reply_photo(photo=photo_url, caption=leaderboard_message, parse_mode='HTML')
+
+application.add_handler(CommandHandler('TopGroups', global_leaderboard, block=False))
+
+application.add_handler(CommandHandler('ctop', leaderboard, block=False))
