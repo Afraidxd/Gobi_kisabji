6ab9ad27c7c96d1f6b7f38f766ed7c9fd755f972
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-18 17:08:37 +0530

    Update rps.py

--- shivu/modules/rps.py
+++ shivu/modules/rps.py
@@ -59,7 +59,7 @@ async def button(update, context):
         result_message = "🎉 You won!"
         await user_collection.update_one({'id': user_id}, {'$inc': {'balance': amount}})
     else:
-        result_message = "😔 You lost!"
+        result_message = "😔 You lost!/nYour updated balance is {'balance}"
         await user_collection.update_one({'id': user_id}, {'$inc': {'balance': -amount}})
 
     await query.message.edit_text(
