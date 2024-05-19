d7452e897185881c82e20dad89274b727b89e978
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-19 09:55:18 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -5,7 +5,7 @@ import html
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from Grabber import (
+from shivu import (
     application, PHOTO_URL, OWNER_ID,
     user_collection, top_global_groups_collection, group_user_totals_collection,
     sudo_users as SUDO_USERS
