c1e9ab0ab8eb13bcff9420beb86334dea4da65a5
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-05-19 14:58:45 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -118,5 +118,3 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
     await harem(update, context, page)
 
 application.add_handler(CommandHandler(["collection"], harem, block=False))
-application.add_handler(CallbackQueryHandler(harem_callback, pattern="^harem:"))
-application.add_handler(CallbackQueryHandler(harem_callback, pattern="^saleslist:close_"))
