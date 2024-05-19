159428e7318c073acfc90309c76ea9c72d0dfde6
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-20 05:07:38 +0530

    Update _callback.py

--- shivu/modules/_callback.py
+++ shivu/modules/_callback.py
@@ -32,8 +32,7 @@ async def cbq(update: Update, context):
         from .spwan import button_click
         await button_click(update, context),
     elif data =='lb_close' :  
-        from .tops import button_handler 
-        await button_handler(update, context),
+                await query.message.delete(),
     elif data.startswith(('help', 'credits', 'back', 'user_help', 'game_help')): 
         await button(update, context)   
 
