04c6c96afac963436be45543e68b5539374269d8
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-05-19 14:42:32 +0530

    Update _callback.py

--- shivu/modules/_callback.py
+++ shivu/modules/_callback.py
@@ -4,7 +4,7 @@ from telegram import Update
 
 from .ptb_store import store_callback_handler, terminate, start_ag
 
-from saleslist import sales_list_callback
+from .saleslist import sales_list_callback
 
 
 async def cbq(update: Update, context):
