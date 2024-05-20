0cea3747d4932098ac334036cd7918cf8e159ed8
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-20 21:51:21 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -12,9 +12,9 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user = await user_collection.find_one({'id': user_id})
     if not user:
         if update.message:
-            await update.message.reply_text('ᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀʀꜱ ɴɪɢɢᴀ...')
+            await update.message.reply_text('ᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀʀꜱ ɴɪɢɢᴀ...')
         else:
-            await update.callback_query.edit_message_text('ᴜ ᴅᴏɴ"ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀʀꜱ ɴɪɢɢᴀ...')
+            await update.callback_query.edit_message_text('ᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀʀꜱ ɴɪɢɢᴀ...')
         return
 
     characters = sorted(user['characters'], key=lambda x: (x['anime'], x['id']))
