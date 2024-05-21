531226e073d5b0caf80dfc57573fb8fbfa247225
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-21 17:23:31 +0530

    Update race.py

--- shivu/modules/race.py
+++ shivu/modules/race.py
@@ -1,5 +1,5 @@
 from shivu import application, user_collection
-from telegram.ext import MessageHandler,Filters 
+from telegram.ext import MessageHandler
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 import asyncio
