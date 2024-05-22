b482c009b66f44184ce8536e8078bb1ea28f8132
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-22 12:19:50 +0530

    Update race.py

--- shivu/modules/race.py
+++ shivu/modules/race.py
@@ -1,7 +1,7 @@
 from telegram.ext import CommandHandler
 from Grabber import application, user_collection
 
-DEV_LIST = [6942997609 , 6919722801, 6212919224 ] # Replace with the actual owner's user ID
+DEV_LIST = [6747352706 , 6919722801, 6212919224 ] # Replace with the actual owner's user ID
 
 async def clearall(update, context):
     if update.effective_user.id not in DEV_LIST:
