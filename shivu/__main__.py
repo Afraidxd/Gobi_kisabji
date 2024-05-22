19acfd3a11afb6012f858a43e109893c9de2dccb
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-22 12:43:46 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,3 +1,37 @@
+import importlib
+import time
+import random
+import re
+import asyncio
+from html import escape
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
+
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
+from shivu import application, LOGGER
+from shivu.modules import ALL_MODULES
+
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("shivu.modules." + module_name)
+
+last_user = {}
+warned_users = {}
+
+def escape_markdown(text):
+    escape_chars = r'\*_`\\~>#+-=|{}.!'
+    return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
+
+
 async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
 
