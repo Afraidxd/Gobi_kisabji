d21fd814e707cff510f0aa604b1d1b423bf2ce91
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-24 03:13:27 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,19 +1,76 @@
-from telegram.ext import ApplicationBuilder
-from shivu import application
+import importlib
+import time
+import random
+import re
+import asyncio
+from html import escape
+
+from typing import Optional
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update
+from telegram.ext import Updater, CallbackQueryHandler
+from telegram.ext import CommandHandler, MessageHandler, filters
+
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
+
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
+from shivu import application, LOGGER
 from shivu.modules import ALL_MODULES
 
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("shivu.modules." + module_name)
+
+last_user = {}
+warned_users = {}
+
+def escape_markdown(text):
+    escape_chars = r'\*_`\\~>#+-=|{}.!'
+    return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
 
 
-def main():
-    # Create the application instance
-    application = ApplicationBuilder().token("6627459799:AAEiY_xENQUklRGc3OWMmwF6rkNdMPkv4OA").build()
+async def fav(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
 
-    # Import and register handlers from your modules
-    for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("shivu.modules." + module_name)  # Ensure your script is imported here if it's a separate module
+    if not context.args:
+        await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙥𝙧𝙤𝙫𝙞𝙙𝙚 𝗖𝗮𝗿 𝙞𝙙...')
+        return
 
-    # Start the bot
-    application.run_polling(drop_pending_updates=True)
+    character_id = context.args[0]
+
+    user = await user_collection.find_one({'id': user_id})
+    if not user:
+        await update.message.reply_text('𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤𝙩 𝙂𝙤𝙩 𝘼𝙣𝙮 𝗖𝗮𝗿 𝙮𝙚𝙩...')
+        return
+
+    character = next((c for c in user['characters'] if c['id'] == character_id), None)
+    if not character:
+        await update.message.reply_text('ᴛʜɪs ᴄᴀʀ ɪs ɴᴏᴛ ɪɴ ʏᴏᴜʀ ᴄᴏʟʟᴇᴄᴛɪᴏɴ')
+        return
+
+    user['favorites'] = [character_id]
+
+    await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}})
+
+    await update.message.reply_text(f'🥳 ᴄᴀʀ {character["name"]} ɪs ʏᴏᴜʀ ғᴀᴠᴏʀɪᴛᴇ ɴᴏᴡ...')
+
+def main() -> None:
+    """Run bot."""
+
+    
+    application.add_handler(CommandHandler(["favorite"], fav, block=False))
+    
+application.run_polling(drop_pending_updates=True)
+
 
 if __name__ == "__main__":
+    shivuu.start()
+    LOGGER.info("Bot started")
     main()
