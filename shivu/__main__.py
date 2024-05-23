8f0d577a964ada32ae7207b373eaa8764abecdf1
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-23 12:25:38 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -4,10 +4,10 @@ import re
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
 from shivu import application, LOGGER
 from shivu.modules import ALL_MODULES
-
+from shivu import shivuu 
 locks = {}
 message_counters = {}
 spam_counters = {}
