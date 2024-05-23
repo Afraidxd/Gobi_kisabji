0ea1dd11b5205be7ea5246fcc123937d90d9b957
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-23 18:15:04 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,4 +1,22 @@
-import logging
+import importlib
+import time
+import random
+import re
+import asyncio
+from html import escape
+
+from typing import Optional
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update
+from telegram.ext import Updater, CallbackQueryHandler
+from telegram.ext import CommandHandler, MessageHandler, filters
+
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
+
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
+from shivu import application, LOGGER
+from shivu.modules import ALL_MODULES
+
 from telegram.ext import Application
 for module_name in ALL_MODULES:
     importlib.import_module(f"shivu.modules.{module_name}")
