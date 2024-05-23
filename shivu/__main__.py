a70f1f87c534318e85a57815a85a72397cfe6d75
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-23 22:19:25 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,8 +1,8 @@
 import importlib
+import asyncio
 import time
 import random
 import re
-import asyncio
 from html import escape
 
 from typing import Optional
@@ -10,8 +10,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import Update
 from telegram.ext import Updater, CallbackQueryHandler
 from telegram.ext import CommandHandler, MessageHandler, filters
-
-from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
 
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
 from shivu import application, LOGGER
@@ -25,16 +23,9 @@ sent_characters = {}
 first_correct_guesses = {}
 message_counts = {}
 
-for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("shivu.modules." + module_name)
-
-last_user = {}
-warned_users = {}
-
 def escape_markdown(text):
     escape_chars = r'\*_`\\~>#+-=|{}.!'
     return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
-
 
 async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
@@ -64,13 +55,15 @@ async def fav(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Run bot."""
 
-    
-    application.add_handler(CommandHandler(["favorite"], fav, block=False))
-    
-application.run_polling(drop_pending_updates=True)
+    for module_name in ALL_MODULES:
+        imported_module = importlib.import_module("shivu.modules." + module_name)
 
+    application.add_handler(CommandHandler(["favorite"], fav, block=False))
+
+    asyncio.create_task(shivuu.start())
+    LOGGER.info("Bot started")
+
+    application.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    shivuu.start()
-    LOGGER.info("Bot started")
     main()
