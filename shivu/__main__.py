3afe9ef5a3c70929704f19e5197ee98d8850b01f
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-23 22:45:24 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -1,12 +1,16 @@
 from telegram.ext import ApplicationBuilder
 from shivu import application
+from shivu.modules import ALL_MODULES
+
+
 
 def main():
     # Create the application instance
     application = ApplicationBuilder().token("6627459799:AAEiY_xENQUklRGc3OWMmwF6rkNdMPkv4OA").build()
 
     # Import and register handlers from your modules
-    import your_module  # Ensure your script is imported here if it's a separate module
+    for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("shivu.modules." + module_name)  # Ensure your script is imported here if it's a separate module
 
     # Start the bot
     application.run_polling(drop_pending_updates=True)
