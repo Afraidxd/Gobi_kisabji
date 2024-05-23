083a92addfa579691662a35145db5d763fdf6695
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-23 08:17:56 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -45,7 +45,7 @@ rarity_map = 1 (⚪️ Common), 2 (🟣 Rare), 3 (🟡 Legendary), 4 (🟢 Mediu
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythic", 6: "🔮 Limited Edition", 7: "🫧 Special"}
+        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythic", 6: "🔮 Limited edition", 7: "🫧 Special"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
@@ -128,7 +128,7 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythic", 6: "🔮 Limited Edition", 7: "🫧 Special"}
+            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Mythic", 6: "🔮 Limited edition", 7: "🫧 Special"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
