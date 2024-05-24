fdba40dcc0ba21a087b6be8b4c05f51a5974b03f
Afraidxd <160904277+Afraidxd@users.noreply.github.com>
2024-05-24 09:25:29 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -5,17 +5,14 @@ import re
 import asyncio
 from html import escape
 
-from typing import Optional
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import Update
-from telegram.ext import Updater, CallbackQueryHandler
-from telegram.ext import CommandHandler, MessageHandler, filters
-
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
 
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
+from shivu import collection,shivuu, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
 from shivu import application, LOGGER
 from shivu.modules import ALL_MODULES
+
 
 locks = {}
 message_counters = {}
@@ -26,10 +23,12 @@ first_correct_guesses = {}
 message_counts = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("shivu.modules." + module_name)
+    imported_module = importlib.import_module("Grabber.modules." + module_name)
+
 
 last_user = {}
 warned_users = {}
+
 
 def escape_markdown(text):
     escape_chars = r'\*_`\\~>#+-=|{}.!'
@@ -40,35 +39,32 @@ async def fav(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
 
     if not context.args:
-        await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙥𝙧𝙤𝙫𝙞𝙙𝙚 𝗖𝗮𝗿 𝙞𝙙...')
+        await update.message.reply_text('𝙋𝙡𝙚𝙖𝙨𝙚 𝙥𝙧𝙤𝙫𝙞𝙙𝙚 Slave 𝙞𝙙...')
         return
 
     character_id = context.args[0]
 
     user = await user_collection.find_one({'id': user_id})
     if not user:
-        await update.message.reply_text('𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤𝙩 𝙂𝙤𝙩 𝘼𝙣𝙮 𝗖𝗮𝗿 𝙮𝙚𝙩...')
+        await update.message.reply_text('𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤𝙩 𝙂𝙤𝙩 𝘼𝙣𝙮 Slave 𝙮𝙚𝙩...')
         return
 
     character = next((c for c in user['characters'] if c['id'] == character_id), None)
     if not character:
-        await update.message.reply_text('ᴛʜɪs ᴄᴀʀ ɪs ɴᴏᴛ ɪɴ ʏᴏᴜʀ ᴄᴏʟʟᴇᴄᴛɪᴏɴ')
+        await update.message.reply_text('𝙏𝙝𝙞𝙨 slave 𝙞𝙨 𝙉𝙤𝙩 𝙄𝙣 𝙮𝙤𝙪𝙧 list')
         return
 
     user['favorites'] = [character_id]
 
     await user_collection.update_one({'id': user_id}, {'$set': {'favorites': user['favorites']}})
 
-    await update.message.reply_text(f'🥳 ᴄᴀʀ {character["name"]} ɪs ʏᴏᴜʀ ғᴀᴠᴏʀɪᴛᴇ ɴᴏᴡ...')
+    await update.message.reply_text(f'🥳slave {character["name"]} is your favorite 𝙣𝙤𝙬...')
+
 
 def main() -> None:
     """Run bot."""
 
     
-    application.add_handler(CommandHandler(["favorite"], fav, block=False))
-    
-application.run_polling(drop_pending_updates=True)
-
 
 if __name__ == "__main__":
     shivuu.start()
