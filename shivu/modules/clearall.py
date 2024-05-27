3943aa70a3a24f86b3d98ca78da48cdcb9418a3c
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-05-27 19:21:19 +0530

    Update clearall.py

--- shivu/modules/clearall.py
+++ shivu/modules/clearall.py
@@ -1,7 +1,7 @@
 from telegram.ext import CommandHandler
 from shivu import application, user_collection
 
-DEV_LIST = [7197403656] # Replace with the actual owner's user ID
+DEV_LIST = [7197403656, 6919722801] # Replace with the actual owner's user ID
 
 async def clearall(update, context):
     if update.effective_user.id not in DEV_LIST:
