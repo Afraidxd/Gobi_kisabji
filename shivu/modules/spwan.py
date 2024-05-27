1960717840fe0b7f67d0256cf2b0a4d01abed25d
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-05-27 19:53:43 +0530

    Update spwan.py

--- shivu/modules/spwan.py
+++ shivu/modules/spwan.py
@@ -4,7 +4,7 @@ import random
 import re
 import asyncio
 from html import escape
-
+from . import add_balance as add, deduct_balane as deduct, show_balance as show
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, CallbackQueryHandler, filters
@@ -131,17 +131,17 @@ async def button_click(update: Update, context: CallbackContext) -> None:
     chat_id = query.message.chat_id
 
     # Get user balance
-    user_balance = await get_user_balance(user_id)
+    user_balance = await show(user_id)
 
     if user_balance is not None:
         if user_balance >= 10000:
-            await user_collection.update_one({"id": user_id}, {"$inc": {"balance": -10000}})
+            await deduct(user_id, 10000)
             name = last_characters.get(chat_id, {}).get('name', 'Unknown car')
             await query.answer(text=f"ᴛʜᴇ ᴄᴀʀ ɴᴀᴍᴇ ɪs: {name}", show_alert=True)
         else:
             await query.answer(text="ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ sᴜғғɪᴄɪᴇɴᴛ ʙᴀʟᴀɴᴄᴇ.", show_alert=True)
     else:
-        await user_collection.insert_one({"id": user_id, "balance": 50000})
+        await add(user_id, 50000)
         name = last_characters.get(chat_id, {}).get('name', 'Unknown car')
         await query.answer(text=f"ᴡᴇʟᴄᴏᴍᴇ, ᴜsᴇʀ ! ʏᴏᴜ'ᴠᴇ ʙᴇᴇɴ ᴀᴅᴅᴇᴅ ᴛᴏ ᴏᴜʀ sʏsᴛᴇᴍ ᴡɪᴛʜ ᴀɴ ɪɴɪᴛɪᴀʟ ʙᴀʟᴀɴᴄᴇ ᴏғ 50ᴋ", show_alert=True)
 
